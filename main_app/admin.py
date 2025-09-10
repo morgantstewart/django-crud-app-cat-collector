ab025e61ff73e25acab62b8a65eb7a1031bc8f54
morgantstewart <morgan@paysigil.com>
2025-09-10 12:34:51 -0400

    added functionality

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-
+from .models import Cat, Feeding
 # Register your models here.
 from django.contrib import admin
 from .models import Cat
--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,37 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
-from django.db import models
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
     age = models.IntegerField()
-    
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        # add the 'choices' field option
+        choices=MEALS,
+        # set the default value for meal to be 'B'
+        default=MEALS[0][0]
+    )
+
+        cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+
     def __str__(self):
-        return self.name
+        return f"{self.get_meal_display()} on {self.date}"
+
+    # Define a method to get the URL for this particular cat instance
+    def get_absolute_url(self):
+        # Use the 'reverse' function to dynamically find the URL for viewing this cat's details
+        return reverse('cat-detail', kwargs={'cat_id': self.id})
